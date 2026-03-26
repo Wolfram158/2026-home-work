183d712c2f5a9b741aa93b1f98c7a811cbe2c394
Wolfram158 <wolframalpha7134@gmail.com>
2026-03-26 17:33:16 +0300

    prevent checks from analyzing integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,6 @@ checkstyle {
     maxWarnings = 0
 }
 
-
 pmd {
     isConsoleOutput = true
     toolVersion = "7.16.0"
@@ -86,12 +85,20 @@ pmd {
     ruleSetFiles(project.layout.projectDirectory.file("pmd.xml"))
 }
 
+tasks.named("checkstyleIntegrationTest").configure {
+    enabled = false
+}
+
+tasks.named("pmdIntegrationTest").configure {
+    enabled = false
+}
+
 tasks.register("codeStyleChecks") {
     group = "verification"
     dependsOn(
         "checkstyleMain",
         "checkstyleTest",
-        "checkstyleIntegrationTest",
+//        "checkstyleIntegrationTest",
         "pmdMain",
     )
 }
