6caa462bc18b43738fef6f4fb63e2ee0eb6b7d21
Wolfram158 <wolframalpha7134@gmail.com>
2026-04-28 22:52:41 +0300

    disable single node test

--- src/integrationTest/java/company/vk/edu/distrib/compute/SingleNodeTest.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/SingleNodeTest.java
@@ -1,6 +1,7 @@
 package company.vk.edu.distrib.compute;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.Parameter;
 import org.junit.jupiter.params.ParameterizedClass;
@@ -13,6 +14,7 @@ import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 @ParameterizedClass
 @ArgumentsSource(KVServiceFactoryArgumentsProvider.class)
 class SingleNodeTest extends TestBase {
